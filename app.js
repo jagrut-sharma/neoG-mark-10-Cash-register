72cfa1f06be4f301399d1c61bd003fc167185462
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-18 18:48:17 +0530

    added a little css and initialised variables in JS

---
+++ app.js
@@ -0,0 +1,9 @@
+const buttonNext = document.querySelector(".btn-nxt");
+const buttonCheck = document.querySelector(".btn-check");
+const billAmount = document.querySelector("#bill-amt");
+const cashGiven = document.querySelector("#cash-gvn");
+const errorMessage = document.querySelector(".error-message");
+const notesNumber = document.querySelectorAll(".notes")
+
+
+// buttonNext.addEventListener("click", () => (notesNumber[1].innerText = 56));
