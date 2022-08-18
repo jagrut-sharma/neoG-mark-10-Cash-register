6549057b0bff052953faaf62338864aabfbe0fe7
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-18 23:20:25 +0530

    until calculateChange

--- app.js
+++ app.js
@@ -7,24 +7,52 @@ const notesNumber = document.querySelectorAll(".notes");
 const enableDisplayOne = document.querySelector(".cash-given");
 const enableDisplayTwo = document.querySelector(".table-container");
 
-let billTotal, cashReceived;
 
-buttonNext.addEventListener("click", function() {
+let billTotal, cashReceived, balanceAmount;
+
+buttonNext.addEventListener("click", 
+    function() {
+
+        setErrorMessage("");
+        billTotal = parseFloat(billAmount.value);
+
+        if (billTotal > 0) {
+            enableDisplayOne.style.display = "block";
+            buttonNext.style.display = "none";        
+        }
+        else{
+            setErrorMessage("Value should be greater than 0");
+        }
+});
+
+buttonCheck.addEventListener("click", 
+function(){
 
     setErrorMessage("");
-    billTotal = billAmount.value;
+    cashReceived = parseFloat(cashGiven.value);
+    billTotal = parseFloat(billAmount.value);
 
-    if (billTotal > 0) {
-        enableDisplayOne.style.display = "block";
-        buttonNext.style.display = "none";        
+    if (cashReceived  > billTotal) {
+        balanceAmount = cashReceived - billTotal;
+        calculateChange(balanceAmount);
+    }
+    else if (cashReceived === billTotal){
+        setErrorMessage("I like you. Have a chocolate 🍫. Now how would you like to pay for it?");
     }
     else{
-        setErrorMessage("Value should be greater than 0");
+        setErrorMessage("Ha Ha! nice one, now pay the bill or else I'll shoot you.");
     }
 });
 
-
-
 function setErrorMessage(message) {
     errorMessage.innerText = message;
+}
+
+function calculateChange(balance){
+
+    const notesValue = [2000, 500, 100, 20, 10, 5, 1];
+    
+    for (let i=0; i<notesNumber.length; i++){
+        console.log(typeof(notesValue[i]),  notesValue[i]);
+    }
 }
