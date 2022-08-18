abab28727ee46f2be566d7d0f294ab2f310f1e15
Jagrut Sharma <jagrut.sharma1@gmail.com>
2022-08-18 20:12:33 +0530

    feat: next button functionality and display properties

--- app.js
+++ app.js
@@ -3,5 +3,28 @@ const buttonCheck = document.querySelector(".btn-check");
 const billAmount = document.querySelector("#bill-amt");
 const cashGiven = document.querySelector("#cash-gvn");
 const errorMessage = document.querySelector(".error-message");
-const notesNumber = document.querySelectorAll(".notes")
+const notesNumber = document.querySelectorAll(".notes");
+const enableDisplayOne = document.querySelector(".cash-given");
+const enableDisplayTwo = document.querySelector(".table-container");
 
+let billTotal, cashReceived;
+
+buttonNext.addEventListener("click", function() {
+
+    setErrorMessage("");
+    billTotal = billAmount.value;
+
+    if (billTotal > 0) {
+        enableDisplayOne.style.display = "block";
+        buttonNext.style.display = "none";        
+    }
+    else{
+        setErrorMessage("Value should be greater than 0");
+    }
+});
+
+
+
+function setErrorMessage(message) {
+    errorMessage.innerText = message;
+}
